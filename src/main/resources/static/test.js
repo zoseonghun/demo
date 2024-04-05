10c5462cf48f59d4cb105c74dcab1461427e9f3c
seonghun <zoseonghun@gmail.com>
2024-04-05 23:34:31 +0900

    .

---
+++ src/main/resources/static/test.js
@@ -0,0 +1,10 @@
+(()=> {
+    // 서버에서 do 테이블의 do 목록을 불러온다
+    // do 목록을 반복해서
+    // id가 do 인 select 태그 안에다 option 태그를 만들어서 넣는다.
+    // id가 do 인 select 태그에 change 이벤트를 건다.
+        // change 됐을때마다 id가 sigungu인 태그 안을 비워준다.
+        // 서버에서 선택된 do_number에 맞는 sigungu 목록을 불러와서
+        // sigungu 목록을 반복해서
+        // id가 sigungu인 태그 안에다 option 태그를 만들어서 넣는다.
+})()
